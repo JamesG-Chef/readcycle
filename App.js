312c3cd661ea95deed97af01b1cd7e71e9553d33
Clayton <clayton.pyne7@gmail.com>
2022-02-19 15:57:09 +0000

    add navigation to App.js

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import BarcodeScanner from "./components/BarcodeScanner";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>ReadCycle</Text>
-      <StatusBar style="auto" />
-      <BarcodeScanner></BarcodeScanner>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Scanner" component={BarcodeScanner} />
+        {/* <View style={styles.container}>
+        <Text>ReadCycle</Text>
+        <StatusBar style="auto" />
+      </View> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
